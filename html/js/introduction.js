739564c1c501bf47eb3e2309bd006283f9377967
Morinaga Keiwa <keiwa.morinaga@esm.co.jp>
2026-04-17 15:13:41 +0900

    js: JavaScript 入門の練習ファイルを追加

---
+++ html/js/introduction.js
@@ -0,0 +1,22 @@
+// console.log("Hello from external file!")
+
+const name = "Taro";
+// console.log(name);
+
+const age = 20;
+console.log(age);
+
+let score = 0;
+console.log(score);
+
+score = 100;
+console.log(score);
+
+// この行は実行されない
+console.log("コメントの下は実行される");
+
+// 複数行コメント
+// あああああ
+// 実行されない
+
+console.log("複数行後も実行される");
