016375b10333792854c7d34a7e4ed274670f59b7
Morinaga Keiwa <keiwa.morinaga@esm.co.jp>
2026-05-19 09:01:45 +0900

    Docker:~2

---
+++ js/js.js
@@ -0,0 +1,11 @@
+const square = (n) => n * n;
+
+console.log(square(4));
+
+const isEven = (n) => n % 2 === 0;
+
+console.log(isEven(10));
+
+const makePoint = (x, y) => ({ x, y });
+
+console.log(makePoint(3, 5));
